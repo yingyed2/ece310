cdfa264f3b35e310ab3bd6e62193a9ad19ab5a18
yingyedeng <90630733+yingyedeng@users.noreply.github.com>
2026-09-20 02:43:53 -0400

    implemented rate conversion

--- implementation1/main.py
+++ implementation1/main.py
@@ -1,14 +1,13 @@
 import numpy as np
 from filters import lpf
-from rate_conversion import upsample
+from rate_conversion import rateConverter
 from utilities import plot_dtft
 
-
 """
-Wc = np.pi / 4
-M = 10    # M = 10 differs from the expected behavior of an ideal lpf with a smoother cutoff; increasing M results in being closer to an ideal lpf
+Wc = np.pi / 160
+M = 100    # M = 10 differs from the expected behavior of an ideal lpf with a smoother cutoff; increasing M results in being closer to an ideal lpf
 
-h = lpf(Wc, M)
+h = 147 * lpf(Wc, M)
 
 plot_dtft(h)
 """
--- implementation1/rate_conversion.py
+++ implementation1/rate_conversion.py
@@ -1,4 +1,5 @@
 import numpy as np
+from filters import lpf
 from scipy.signal import lfilter
 
 
@@ -20,5 +21,19 @@ def downsample(x, D):
 def filterSignal(x,h):
 
     y = lfilter(h, 1, x)
+
+    return y
+
+
+def rateConverter(x, M):
+
+    L = 147
+    D = 160
+
+    h = 147 * lpf(np.pi / 160, M)
+
+    xUpsampled = upsample(x, L)
+    xFiltered = filterSignal(xUpsampled, h)
+    y = downsample(xFiltered, D)
 
     return y
